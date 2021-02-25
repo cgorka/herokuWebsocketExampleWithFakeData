4803e82efaa1f8de6db23f13220ebce0c30d0e0a
cgorka <cgorka@gmail.com>
2021-02-25 23:57:44 +0100

    1

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ function getItem(id) {
             "first_name": faker.name.firstName() +' '+faker.name.lastName(),
             "last_name": faker.name.findName(),
             "email": faker.internet.email(),
-            "genre": faker.music.genre(),
+            "genre": faker.lorem.word(),
             "ip_address": faker.internet.ip(),
             "mass":faker.finance.amount(0,100,4)+" g"
         }
